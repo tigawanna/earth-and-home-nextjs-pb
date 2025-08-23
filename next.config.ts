6c66e36dbe13e57085e17a7aaf9811ad4042ed49
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-23 11:14:01 +0300

    cleanup

--- next.config.ts
+++ next.config.ts
@@ -12,22 +12,12 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
       },
-      {
-        protocol: "https",
-        hostname: process.env.PB_ORIGIN || "",
-      },
     ],
   },
   experimental: {
     reactCompiler: true,
     viewTransition: true,
   },
-  rewrites: async () => [
-    {
-      source: "/dashboard/:path*",
-      destination: "/dashboard",
-    },
-  ],
 };
 
 export default nextConfig;
