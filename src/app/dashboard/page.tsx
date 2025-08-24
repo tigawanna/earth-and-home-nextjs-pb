1a9c6fd4974308cdeae921a3415c72988437302e
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-24 23:28:39 +0300

    refactor: Add searchParams prop to DashboardPage component; create admin-stats.ts file for future implementation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,9 @@
-export default function DashboardPage() {
+interface DashboardPageProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function DashboardPage({ searchParams }: DashboardPageProps) {
+  const sp = await searchParams;
   return (
     <section className="w-full h-full  flex flex-col items-center justify-center">
       Dashboard Page
