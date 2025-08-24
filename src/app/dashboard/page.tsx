5d84b671f94f5b2328c0cdfdd8db11ade927925f
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-24 23:35:27 +0300

    refactor: Add user authentication to DashboardPage component; integrate server-side user retrieval

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import { getServerSideUser } from "@/data-access-layer/pocketbase/user/server-side-auth";
+
 interface DashboardPageProps {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export default async function DashboardPage({ searchParams }: DashboardPageProps) {
   const sp = await searchParams;
+    const user = await getServerSideUser();
   return (
     <section className="w-full h-full  flex flex-col items-center justify-center">
       Dashboard Page
