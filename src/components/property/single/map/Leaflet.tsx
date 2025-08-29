206e31c6551603716ba96bdf592cf0e3e6b62b72
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-29 05:50:36 +0300

    feat: Add initial Leaflet component with basic structure and styling

---
+++ src/components/property/single/map/Leaflet.tsx
@@ -0,0 +1,11 @@
+interface LeafletProps {
+  
+}
+
+export function Leaflet({  }: LeafletProps) {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+        Leaflet
+    </div>
+  );
+}
