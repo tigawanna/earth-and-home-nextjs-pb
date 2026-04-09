2fd05572ed27511580c2dcdcc47e909233feb6a0
tigawanna <denniskinuthiaw@gmail.com>
2026-04-09 21:21:18 +0300

    chore: clean up drizzle.config.ts by removing unnecessary whitespace
    - Removed extra whitespace in drizzle.config.ts for improved code readability.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,6 @@ const localD1Sqlite =
   process.env.DRIZZLE_STUDIO_DB_URL ??
   "file:.wrangler/state/v3/d1/miniflare-D1DatabaseObject/2981305cef37cdd243afba27f7a6a3a923b95a618d4a7a614789755fb76a5742.sqlite";
 
-  
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
